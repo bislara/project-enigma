8e3338ec0bde2c91c37edbcbd0af3458ed2b0854
Subham3896 <biswajeetsahoo54@gmail.com>
2021-07-13 19:11:59 +0530

    home page done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-import homeAnime from './../../assets/home_anime.gif';
+import homeAnime from './../../assets/home_anime_3.gif';
 import './Home.css';
 
 class Home extends Component {
@@ -20,26 +20,32 @@ class Home extends Component {
     render() {
         return (
             <div className='home-container'>
-                
-                <div className='header-text'>
-                    <h1>Welcome to my portfolio!</h1>
-                    <p>This is Madhav, a full stack dev and a teacher</p>
+                <div className='home-left'>
+                    <div className='header-text'>
+                        <h3>Hello World!</h3>
+                        <h1>I'm Biswajeet Sahoo.</h1>
+                        <p>Full Stack Developer who's ready to turn Ideas into Reality. </p>
+                    </div>
+                    <div className='head-btns'>
+                        <Link to='/about' className='btn btn-white'>
+                            <p className='btn-text'>Know more about me</p>
+                        </Link>
+                        <Link to='/contact' className='btn btn-transparent'>
+                            <p className='btn-text'>Connect with me</p>
+                        </Link>
+                    </div>
                 </div>
-                <div className='head-btns'>
-                    <Link to='/about' className='btn btn-white'>
-                        <p className='btn-text'>Know more about me</p>
-                    </Link>
-                    <Link to='/contact' className='btn btn-transparent'>
-                        <p className='btn-text'>Connect with me</p>
-                    </Link>
+
+                <div className='home-right'>
+                    <div className='splash-image'>
+                        <img
+                            src={homeAnime}
+                            alt='animation'
+                            className='home-anime'
+                        />
+                    </div>
                 </div>
-                <div className='splash-image'>
-                    <img
-                        src={homeAnime}
-                        alt='animation'
-                        className='home-anime'
-                    />
-                </div>
+
             </div>
         );
     }
