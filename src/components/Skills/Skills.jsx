4dc0a3c3d8797e6724b59f53f13950321e14f7d5
Subham3896 <biswajeetsahoo54@gmail.com>
2021-07-15 19:44:48 +0530

    final changes done to the skills page

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import FooterLink from '../FooterLink/FooterLink';
 import Header from '../Header/Header';
 import { skillList } from './../../assets/skillsData';
 import './Skills.css';
 import skillsVector from './../../assets/skills_vector.png';
 import SkillCard from './SkillCard';
+
+import DisplayLottie from './DisplayLottie'
+import webdev from './../../assets/lottie/webdev.json';
+
+const SkillBars = [
+    {
+      Stack: 'Frontend/Design', //Insert stack or technology you have experience in
+      progressPercentage: '65', //Insert relative proficiency in percentage
+    },
+    {
+      Stack: 'Backend',
+      progressPercentage: '75',
+    },
+    {
+      Stack: 'Programming',
+      progressPercentage: '80',
+    },
+  ]
+
 
 const Skills = () => {
     return (
@@ -14,13 +33,42 @@ const Skills = () => {
                 details="Passionate about new technologies, I keep exploring stuff. Here's the tech stack I've worked with!"
             />
 
-            <div className='skill-card-container'>
+            <div className='skill-index'>    
                 {skillList.map(({ skillName, skillUrl }) => (
-                    <SkillCard skillName={skillName} skillUrl={skillUrl} />
-                ))}
+                        <SkillCard skillName={skillName} skillUrl={skillUrl} />
+                    ))}
             </div>
 
-            <FooterLink phrase='Get in ' link='touch.' toAdress='/contact' />
+            <div className='skill-card-container'>
+
+                <div className='skills-left'>
+                        <DisplayLottie animationData={webdev} />                
+                    </div>
+
+                <div className='skills-right'>
+                    
+                    <h1 className="h1">Proficiency</h1>
+                        {
+                            SkillBars.map(skill => {
+                                return <div className="wrapper">
+                                <div className="progress-label">
+                                    <span>{skill.Stack}</span>
+                                    </div>
+                                    
+                                <div className="progress-bar">    
+                                  <div className="bar" data-size={skill.progressPercentage}>
+                                    <span className="perc"></span>
+                                  </div>
+                                </div>
+                              </div>
+                            })
+                        }
+
+                </div>
+
+            </div>
+
+            <FooterLink phrase='Check out my ' link='projects!' toAdress='/projects' />
 
             <div className='skills-vector-frame'>
                 <img
@@ -33,4 +81,31 @@ const Skills = () => {
     );
 };
 
+function loading() {
+    document.querySelectorAll(".bar").forEach(function(current) {
+      let startWidth = 0;
+      const endWidth = current.dataset.size;
+      console.log("width is: ",endWidth);
+      
+      /* 
+      setInterval() time sholud be set as trasition time / 100. 
+      In our case, 2 seconds / 100 = 20 milliseconds. 
+      */
+      const interval = setInterval(frame, 20);
+      function frame() {
+        if (startWidth >= endWidth) {
+          clearInterval(interval);
+        } else {
+            startWidth++;
+            current.style.width = `${endWidth}%`;
+            current.firstElementChild.innerText = `${startWidth}%`;
+          }
+       }
+    });
+  }
+  
+  setTimeout(loading, 1000);
+
+
+  
 export default Skills;
