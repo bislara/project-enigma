9f1a90611c1a2a9de8722b0b70f03c85680c8a51
Subham3896 <biswajeetsahoo54@gmail.com>
2021-08-01 23:41:26 +0530

    contact us page done

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,8 +6,11 @@ import contactVector from './../../assets/contact_anime.png';
 import github from './../../assets/gh.png';
 import linkedin from './../../assets/li.png';
 import instagram from './../../assets/in.png';
-import medium from './../../assets/me.png';
+import fb from './../../assets/fbIcon.png';
 import web from './../../assets/web.png';
+import phone from './../../assets/phoneIcon.png';
+import email from './../../assets/emailIcon.png';
+import address from './../../assets/addressIcon.png';
 
 const Contact = () => {
     return (
@@ -19,15 +22,41 @@ const Contact = () => {
 
             {/* Form section */}
             <div className='contact-form-container'>
-                <form className='contact-form'>
+                <form 
+                    className='contact-form' 
+                    name="contactForm"
+                    method="post"
+                    action="https://formspree.io/f/moqpeeee"
+                >
+
+                    <input
+                        name="contactName"
+                        type="text"
+                        id="contactName"
+                        placeholder="Your Name"
+                        minlength="2"
+                        className='input-box email-input'
+                        required={true}
+                        autoComplete='off'
+                    />  
                     {/* Email Id Input */}
                     <input
                         type='email'
+                        id="contactEmail"
                         placeholder='Your Email ID'
-                        name='email'
+                        name="_replyto"
                         className='input-box email-input'
                         autoComplete='off'
+                        required={true}
                     />
+
+                    <input
+                        name="contactSubject"
+                        type="text"
+                        id="contactSubject"
+                        placeholder="Subject"
+                        className='input-box email-input'
+                        />
 
                     {/* Email Body */}
                     <textarea
@@ -35,40 +64,63 @@ const Contact = () => {
                         placeholder='Your Message'
                         name='message'
                         className='input-box body-input'
+                        required ={true}
                     ></textarea>
 
                     {/* Submit button */}
-                    <button type='submit' className='contact-btn'>
+                    <button type='submit' className='btn-grad'>
                         Send Email
                     </button>
                 </form>
             </div>
 
+            <div className="details-container">
+
+                <div className="details">
+                    <img src={address} style={{width: "80px",marginLeft: "auto",marginRight: "auto"}}/>
+                    <h5>Where to Find Me</h5>
+                    <p>Flat no- 4/A, Chiranjiv Complex, Mangalabag, Cuttack, Odisha, India, Pin Code-753001</p>
+                </div>
+
+                <div className="details">
+                    <img src={email} style={{width: "80px",marginLeft: "auto",marginRight: "auto"}}/>
+                    <h5>Email me at</h5>
+                    <p>biswajeetsahoo54@gmail.com</p>
+                </div>
+                
+                <div className="details">
+                    <img src={phone} style={{width: "80px",marginLeft: "auto",marginRight: "auto"}}/>
+                    <h5>Call me at</h5>
+                    <p>Phone: (+91) 97787 56623</p>
+                </div>
+                
+            </div>
+
             {/* Social icons */}
             <div className='social-icons-container'>
-                <a href='https://github.com/MadhavBahl' className='social-icon'>
-                    <img src={github} alt='social' />
+                <a href='https://github.com/bislara' target="_blank" className='social-icon'>
+                    <img src={github} alt='social' style={{height: "50px"}}/>
                 </a>
                 <a
-                    href='https://linkedin.com/in/MadhavBahl'
+                    href='https://www.linkedin.com/in/biswajeet-sahoo-lara/'
                     className='social-icon'
+                    target="_blank"
                 >
-                    <img src={linkedin} alt='social' />
+                    <img src={linkedin} alt='social' style={{height: "50px"}}/>
                 </a>
                 <a
-                    href='https://instagram.com/theleanprogrammer'
+                    href='https://www.instagram.com/bis_lara/'
                     className='social-icon'
+                    target="_blank"
                 >
-                    <img src={instagram} alt='social' />
+                    <img src={instagram} alt='social' style={{height: "50px"}}/>
                 </a>
                 <a
-                    href='https://medium.com/@madhavbahl'
+                    href='https://www.facebook.com/biswajeet.sahoo.lara/'
                     className='social-icon'
+                    target="_blank"
                 >
-                    <img src={medium} alt='social' />
-                </a>
-                <a href='https://madhavbahl.tech' className='social-icon'>
-                    <img src={web} alt='social' />
+                    <img src={fb} alt='social' style={{height: "50px"}}/>
                 </a>
             </div>
 
